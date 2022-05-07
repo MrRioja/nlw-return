e9bde34d2d1212bc2ef7b566a659593884f12e7c
Luiz Rioja <lulyrioja@gmail.com>
2022-05-06 21:09:08 -0300

    chore: create nodemailer-mail-adapter

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "59daecf4671a46",
+    pass: "e24ecd841b29e7",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Luiz Rioja <lulyrioja@gmail.com>",
+      subject: subject,
+      html: body,
+    });
+  }
+}
