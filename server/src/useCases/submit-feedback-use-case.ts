e55cfdc0c6bc5b8dc9ef5a201c6170d615860049
Luiz Rioja <lulyrioja@gmail.com>
2022-05-11 20:29:39 -0300

    chore: add screenshot in email body

--- server/src/useCases/submit-feedback-use-case.ts
+++ server/src/useCases/submit-feedback-use-case.ts
@@ -37,9 +37,10 @@ export class SubmitFeedbackUseCase {
     await this.mailAdapter.sendMail({
       subject: "Novo feedback",
       body: [
-        `<div style="font-family: sans-serif; font-size: 16px; color: #111;" >`,
+        `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
         `<p>Tipo do feedback: ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
+        screenshot ? `<img src="${screenshot}" />` : ``,
         `</div>`,
       ].join("\n"),
     });
