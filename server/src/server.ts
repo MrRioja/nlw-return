44f66d680a8da31e551043dff3467ce5fe938809
Luiz Rioja <lulyrioja@gmail.com>
2022-05-13 21:05:56 -0300

    fix: add header in response

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,12 +4,13 @@ import { routes } from "./routes";
 
 const app = express();
 
-app.use(
-  cors({
-    allowedHeaders: ["Access-Control-Allow-Credentials"],
-    origin: "*",
-  })
-);
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(express.json());
 
 app.use(routes);
